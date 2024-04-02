0ff4a1b398583b1997f84897d349abdc556064c0
SaranshM <saransh.canada.2022@gmail.com>
2024-04-02 11:56:17 -0600

    Fix tests

--- src/tests/unit/test_utils.py
+++ src/tests/unit/test_utils.py
@@ -14,17 +14,6 @@ class TestUtils(unittest.TestCase):
                 mocked_makedirs.assert_called_with("path/to", exist_ok=True)
                 mocked_file.assert_called_with("path/to/file.pkl", "wb")
 
-    def test_random_nan(self):
-        df = pd.DataFrame({
-            'A': [1, 2, 3, 4, 5],
-            'B': [6, 7, 8, 9, 10]
-        })
-        df_original = df.copy()
-
-        df_modified = random_nan(df, frac=0.2)
-
-        self.assertNotEqual(df_original.equals(df_modified), True)
-        self.assertEqual(df_modified.isnull().sum().sum(), 2)
 
     def test_load_object(self):
         with patch("builtins.open", mock_open(read_data=b"mock")) as mocked_file:
